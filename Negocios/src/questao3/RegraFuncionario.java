5be2136ff3fe86a1305f398f515d50ee2d67c2c7
MyllenaSales <160550269+MyllenaSales@users.noreply.github.com>
2024-03-06 08:36:48 -0300

    Update RegraFuncionario.java

--- Negocios/src/questao3/RegraFuncionario.java
+++ Negocios/src/questao3/RegraFuncionario.java
@@ -4,11 +4,13 @@ public class RegraFuncionario {
 	
 	Funcionario[] f = new Funcionario[2];
 	
+	int i =0;
+	
 	public void regraFuncionario(Funcionario funcionarios) {
-		
-		for(int i=0;i<f.length;i++) {
 			f[i] = funcionarios;
-		}
+			
+			i++;
+	
 	}
 	
 	public Funcionario[] dadosFuncionario() {
